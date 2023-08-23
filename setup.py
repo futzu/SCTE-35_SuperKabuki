9fe5780d7c432d14273d7004cf54fc309884c5f9
Adrian <52701496+futzu@users.noreply.github.com>
2023-08-23 00:10:18 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setuptools.setup(
     scripts=["bin/superkabuki"],
     platforms="all",
     install_requires=[
-        "threefive >= 2.3.71",
-        "new_reader >= 0.1.3",
-        "iframes",
+        "threefive >= 2.4.1",
+        "new_reader >= 0.1.7",
+        "iframes >= 0.0.7",
     ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
